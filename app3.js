c5fc63795a809ad0578d9558b8473f0dbb96eb8c
ktkelley <kevinthomaskelley@gmail.com>
2020-10-02 19:54:59 -0600

    js code added

---
+++ app3.js
@@ -0,0 +1,12 @@
+
+
+function greeting(phrase) {
+    console.log(phrase)
+}
+let sentence = "Hello Everybody! This is my first argument."
+greeting(sentence)
+//arguments are always supplied by value
+greeting("Hello World!")
+
+document.write("Hello World!");
+alert("Welcome to my site!");
